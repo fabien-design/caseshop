718692c9b8f58d55643ab418b5c2e5d0e20ad557
fabien-design <fabienrozier60@gmail.com>
2024-06-17 11:54:09 +0200

    chore: Update copyright year in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="h-full flex flex-col justify-center items-center">
                 <div className="text-center md:text-left pb-2 md:pb-0">
                     <p className="text-sm text-muted-foreground">
-                        &copy; 2024 CaseCobra. All rights reserved.
+                        &copy; {new Date().getFullYear()} CaseCobra. All rights reserved.
                     </p>
                 </div>
             </div>    
