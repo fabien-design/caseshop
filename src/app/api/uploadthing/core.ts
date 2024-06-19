bf4cfbcc168989593d80e5c2e8ab7003cf1061ac
fabien-design <fabienrozier60@gmail.com>
2024-06-19 17:52:30 +0200

    chore: Add Page component for configuring design
    chore: Add config for img in DB

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,8 +1,10 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
- 
+import sharp from "sharp";
+import { db } from "@/db";
+
 const f = createUploadthing();
- 
+
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "16MB" } })
@@ -12,8 +14,36 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const configId = metadata.input.configId;
-      return { configId };
+
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+
+      const imgMetadata = await sharp(buffer).metadata();
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imgUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+            where: {
+                id: configId
+            },
+            data: {
+                croppedImgUrl: file.url,
+            }
+        })
+        return { configId: updatedConfiguration.id }
+      }
+
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
